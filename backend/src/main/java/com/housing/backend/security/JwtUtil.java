57609504a5adcb52933be24b383cc8cf656c21f5
andyjianzhou <andyzhou727@gmail.com>
2025-03-27 01:22:06 -0600

    Setting up JWTS

---
+++ backend/src/main/java/com/housing/backend/security/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.housing.backend.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "supersecretkeysupersecretkeysupersecretkey";
+    private final long EXPIRATION_TIME = 86400000;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    // Generate token
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    // Validate Token
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+}
